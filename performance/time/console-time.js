80fb276a8e4f1bfbec791bf9be12d2bedbfe4a4e
Doniel Acosta <donielacosta1995@gmail.com>
2024-02-13 20:36:58 -0400

    ordenamiento con big o

--- performance/time/console-time.js
+++ performance/time/console-time.js
@@ -1,9 +1,13 @@
+
 function contar(n) {
   for (let i = 0; i < n; i++) {
     console.log(i);
   }
 }
 
-console.time("duracion-contar");
+console.time('duracion del algoritmo contar');
 contar(5);
-console.timeEnd("duracion-contar");
+console.timeEnd('duracion del algoritmo contar');
+
+
+
